e4298ad9dfa24e477b25d63a7e5a24e7f583bbf9
Student User <student@cti.pub.ro>
2016-04-01 16:33:56 +0300

    to_services

--- PracticalTest01Var062/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
+++ PracticalTest01Var062/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01var06/PracticalTest01Var06MainActivity.java
@@ -2,15 +2,99 @@ package ro.pub.cs.systems.eim.practicaltest01var06;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class PracticalTest01Var06MainActivity extends ActionBarActivity {
+
+    private Button mDetailsButton;
+    private Button mPassFailButton;
+    private RelativeLayout mCenterRl;
+    private EditText mLinkEditText;
+    private EditText mTopEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var06_main);
+
+        mDetailsButton = (Button) findViewById(R.id.details_button);
+        mCenterRl = (RelativeLayout) findViewById(R.id.center_rel_layout);
+        mLinkEditText = (EditText) findViewById(R.id.link_edit_text);
+        mPassFailButton = (Button) findViewById(R.id.pass_fail_button);
+        mTopEditText = (EditText) findViewById(R.id.top_edit_text);
+
+        mDetailsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCenterRl.getVisibility() == View.VISIBLE) {
+                    mCenterRl.setVisibility(View.INVISIBLE);
+                    mDetailsButton.setText("More details");
+                } else {
+                    mCenterRl.setVisibility(View.VISIBLE);
+                    mDetailsButton.setText("Less details");
+                }
+
+            }
+        });
+
+        mLinkEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                String editTextString = mLinkEditText.getText().toString();
+                if (editTextString.length() >= 4) {
+                    if ("http".equals(editTextString.substring(0, 4))) {
+                        mPassFailButton.setText("Pass");
+                        mPassFailButton.setBackground(getResources().getDrawable(R.color.green));
+                    } else {
+                        mPassFailButton.setText("Fail");
+                        mPassFailButton.setBackground(getResources().getDrawable(R.color.red));
+                    }
+                } else {
+                    mPassFailButton.setText("Fail");
+                    mPassFailButton.setBackground(getResources().getDrawable(R.color.red));
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString("top_edit_text", mTopEditText.getText().toString());
+        savedInstanceState.putString("details_button", mDetailsButton.getText().toString());
+        savedInstanceState.putString("pass_fail_button", mPassFailButton.getText().toString());
+        savedInstanceState.putString("link_edit_text", mLinkEditText.getText().toString());
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        String top = savedInstanceState.getString("top_edit_text");
+        String detailsButton = savedInstanceState.getString("details_button");
+        String passFail = savedInstanceState.getString("pass_fail_button");
+        String link = savedInstanceState.getString("link_edit_text");
+
+        Toast.makeText(getApplicationContext(), top + "\n" + detailsButton + "\n" + passFail + "\n" + link, Toast.LENGTH_LONG).show();
+
     }
 
     @Override
